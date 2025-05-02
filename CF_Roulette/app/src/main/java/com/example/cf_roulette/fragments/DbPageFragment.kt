3e93387b810cc62f9a99e2df703771cb264e90fa
MikhailKapitov <mkapitov4@gmail.com>
2025-05-02 22:14:24 +0500

    DB page is now functional!

--- CF_Roulette/app/src/main/java/com/example/cf_roulette/fragments/DbPageFragment.kt
+++ CF_Roulette/app/src/main/java/com/example/cf_roulette/fragments/DbPageFragment.kt
@@ -2,13 +2,17 @@ package com.example.cf_roulette.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.cf_roulette.R
+import com.example.cf_roulette.repository.ProblemRepository
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -16,6 +20,7 @@ class DbPageFragment : Fragment() {
     private lateinit var dbHash1: TextView
     private lateinit var dbHash2: TextView
     private lateinit var lastUpdText: TextView
+    private lateinit var problemRepository: ProblemRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,7 @@ class DbPageFragment : Fragment() {
         dbHash1 = view.findViewById(R.id.db_hash_1)
         dbHash2 = view.findViewById(R.id.db_hash_2)
         lastUpdText=view.findViewById(R.id.text_last_update)
+        problemRepository = ProblemRepository.getInstance(requireContext())
 
         val updateButton: Button = view.findViewById(R.id.btn_last_update)
         updateButton.setOnClickListener {
@@ -33,18 +39,50 @@ class DbPageFragment : Fragment() {
         }
 
         showSavedTime()
+        displayHash()
 
         return view
     }
 
-    private fun updateHash() {
-        dbHash1.text = "Static hash 1"
-        dbHash2.text = "Static hash 2"
+    private fun displayHash(){
 
-        val currentTime = System.currentTimeMillis()
-        val sharedPrefs = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        sharedPrefs.edit().putLong("last_click_time", currentTime).apply()
-        showSavedTime()
+        viewLifecycleOwner.lifecycleScope.launch {
+
+            var problemset = problemRepository.getProblemset(800, 3500, 0, listOf(), specialTagBanned = false, tagOring = false)
+            if (problemset == null){
+                dbHash1.text = "!!!"
+                dbHash2.text = "!!!"
+                return@launch
+            }
+            dbHash1.text = problemRepository.hashProblemList(problemset) + " (" + problemset.size.toString() + ")"
+
+            problemset = problemRepository.getProblemset(800, 3500, 128, listOf(), specialTagBanned = false, tagOring = false)
+            if (problemset == null){
+                dbHash2.text = "!!!"
+                return@launch
+            }
+            dbHash2.text = problemRepository.hashProblemList(problemset) + " (" + problemset.size.toString() + ")"
+
+        }
+
+    }
+
+    private fun updateHash() {
+
+        viewLifecycleOwner.lifecycleScope.launch {
+
+            if(!problemRepository.updateCache()){
+                return@launch
+            }
+
+            val currentTime = System.currentTimeMillis()
+            val sharedPrefs = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+            sharedPrefs.edit().putLong("last_click_time", currentTime).apply()
+            showSavedTime()
+
+            displayHash()
+        }
+
     }
 
     private fun showSavedTime() {
